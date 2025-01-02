0dc9caadda3974347e95da2c5a528afd3bbfa737
Naveen <naveennynlp@gmail.com>
2025-01-01 19:12:39 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,242 @@
+import json
+import os
+import boto3
+from dotenv import load_dotenv
+import streamlit as st
+import fmpsdk
+
+load_dotenv()
+
+# Initialize AWS Bedrock client (for model interactions, if needed)
+modelId = 'anthropic.claude-3-sonnet-20240229-v1:0'
+api_key = os.getenv("FINANCIAL_MODELING_PREP_API_KEY")
+bedrock = boto3.client(
+    service_name='bedrock-runtime',
+    region_name='us-east-1',
+)
+
+toolConfig = {'tools': [], 'toolChoice': {'auto': {}}}
+
+# Define the ToolsList class with tool functions
+class ToolsList:
+    def get_stock_price(self, symbol: str):
+        """
+        Fetch the current stock price and other relevant data for the given symbol.
+        Uses the fmpsdk to fetch stock quote data.
+        """
+        try:
+            # Fetch stock data using fmpsdk quote
+            stock = fmpsdk.quote(apikey=api_key, symbol=symbol)
+            
+            # Extract the first item from the list (stock data)
+            if stock:
+                stock = stock[0]
+                return json.dumps(stock)
+            else:
+                return {"error": f"No data found for symbol: {symbol}"}
+
+        except Exception as e:
+            return {"error": f"An error occurred: {str(e)}"}
+
+    def get_company_financials(self, symbol):
+        try:
+            # Fetch stock data using fmpsdk quote
+            financials = fmpsdk.company_profile(apikey=api_key, symbol=symbol)
+            
+            # Extract the first item from the list (stock data)
+            if financials:
+                financials = financials[0]
+                return json.dumps(financials)
+            else:
+                return {"error": f"No data found for symbol: {symbol}"}
+
+        except Exception as e:
+            return {"error": f"An error occurred: {str(e)}"}
+
+
+    def get_income_statement(self, symbol):
+        try:
+            # Fetch stock data using fmpsdk quote
+            stock = fmpsdk.quote(apikey=api_key, symbol=symbol)
+            
+            # Extract the first item from the list (stock data)
+            if stock:
+                stock = stock[0]
+                return json.dumps(stock)
+            else:
+                return {"error": f"No data found for symbol: {symbol}"}
+
+        except Exception as e:
+            return {"error": f"An error occurred: {str(e)}"}
+    
+# Tool Configuration
+toolConfig['tools'].append({
+    'toolSpec': {
+        'name': 'get_stock_price',
+        'description': 'Get the current stock price for a given company symbol.',
+        'inputSchema': {
+            'json': {
+                'type': 'object',
+                'properties': {
+                    'symbol': {
+                        'type': 'string',
+                        'description': 'The stock ticker symbol, e.g., AAPL for Apple.'
+                    }
+                },
+                'required': ['symbol']
+            }
+        }
+    }
+})
+
+
+toolConfig['tools'].append({
+    'toolSpec': {
+        'name': 'get_company_financials',
+        'description': 'Get basic financial information for a given company.',
+        'inputSchema': {
+            'json': {
+                'type': 'object',
+                'properties': {
+                    'symbol': {
+                        'type': 'string',
+                        'description': 'The stock ticker symbol, e.g., GOOGL for Alphabet Inc.'
+                    }
+                },
+                'required': ['symbol']
+            }
+        }
+    }
+})
+
+
+
+toolConfig['tools'].append({
+    'toolSpec': {
+        'name': 'get_income_statement',
+        'description': 'Get the last income statement for a given company symbol.',
+        'inputSchema': {
+            'json': {
+                'type': 'object',
+                'properties': {
+                    'symbol': {
+                        'type': 'string',
+                        'description': 'The stock ticker symbol, e.g., AMZN for Amazon.'
+                    }
+                },
+                'required': ['symbol']
+            }
+        }
+    }
+})
+
+# Function for orchestrating the conversation flow
+def converse(prompt, system=''):
+    # Add the initial prompt:
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "text": prompt
+                }
+            ]
+        }
+    ]
+    
+    print(f"Initial prompt:\n{json.dumps(messages, indent=2)}")
+
+    # Invoke the model the first time
+    output = converse_with_tools(messages, system)
+    print(f"Output so far:\n{json.dumps(output['output'], indent=2, ensure_ascii=False)}")
+
+    # Add the intermediate output to the prompt:
+    messages.append(output['output']['message'])
+
+    # Check if function calling is triggered
+    function_calling = next((c['toolUse'] for c in output['output']['message']['content'] if 'toolUse' in c), None)
+
+    if function_calling:
+        # Get the tool name and arguments
+        tool_name = function_calling['name']
+        tool_args = function_calling['input'] or {}
+
+        # Run the tool
+        print(f"Running ({tool_name}) tool...")
+        print("tool_args:", tool_args)
+        print(f"Calling {tool_name} with arguments {tool_args}")
+
+        # Run the tool with the symbol argument
+        tool_response = getattr(ToolsList(), tool_name)(tool_args['symbol'])
+        tool_status = 'success' if tool_response else 'error'
+
+        # Add the tool result to the prompt
+        messages.append(
+            {
+                "role": "user",
+                "content": [
+                    {
+                        'toolResult': {
+                            'toolUseId': function_calling['toolUseId'],
+                            'content': [
+                                {
+                                    "text": tool_response
+                                }
+                            ],
+                            'status': tool_status
+                        }
+                    }
+                ]
+            }
+        )
+
+        # Invoke the model one more time to finalize the conversation
+        output = converse_with_tools(messages, system)
+        print(f"Final output:\n{json.dumps(output['output'], indent=2, ensure_ascii=False)}\n")
+
+    return output['output']
+
+
+# Function for calling the Bedrock Converse API
+def converse_with_tools(messages, system='', toolConfig=toolConfig):
+    response = bedrock.converse(
+        modelId=modelId,
+        system=system,
+        messages=messages,
+        toolConfig=toolConfig
+    )
+    return response
+
+def chat_app():
+    st.title("Market Assistant")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        system_message = [{"text": "You're provided with tools that can fetch the current stock price for a given company symbol ('get_stock_price'), basic financial information for a given company ('get_company_financials'), and the last income statement for a company ('get_income_statement'). \
+                Use these tools if necessary to answer the question. Do not mention the tools in your final answer."}]
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Get assistant response from the bot
+        response = converse(prompt=prompt, system=system_message).get("message", {}).get("content", [{}])[0].get("text", "No response received.")
+
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+
+if __name__ == "__main__":
+    chat_app()
---
+++ readme.md
@@ -0,0 +1,58 @@
+**Market Assistant: Stock and Financial Data Chatbot**
+
+This is a market data assistant application built using Streamlit, AWS Bedrock Converse, FMPSDK, and other supporting libraries. The chatbot leverages Anthropic Claude Sonnet model and interacts with financial data APIs to provide real time stock prices, financials, and income statements for various companies.
+
+**Features**
+
+-   Real-time Stock Price: Fetch the current stock price of a given symbol (e.g., AAPL for Apple).
+-   Company Financial Information: Get basic financial details for a specific company.
+-   Income Statement: Retrieve the latest income statement of a company.
+
+**Prerequisites**
+
+Before running the code, make sure to complete the following:
+
+-   [AWS CLI Setup](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) : Install and configure the AWS Command Line Interface (CLI) with the appropriate credentials.
+
+-   AWS Bedrock Access: You must have access to Amazon Bedrock and the appropriate credentials to use its models, including the Anthropic Claude Sonnet model.
+
+-   Financial Modelling Prep Setup: You need to have a valid financialmodelingprep API key to access the real time financial data. You can get it from [financialmodelingprep](https://site.financialmodelingprep.com/)
+
+**Setup**
+
+1. Clone the Repository
+
+    ```
+    git clone https://github.com/stackmodel/market-assistant.git
+    cd market-assistant
+    ```
+2. Rename .env.example to .env file in the root of your project to store your financialmodelingprep api key.
+
+    ```FINANCIAL_MODELING_PREP_API_KEY=your_api_key_here```
+3. Make sure you have Python 3.12 or higher installed. Then, create a virtual environment and install the dependencies:
+    
+    ```python3 -m venv venv```
+
+4. Run the following to install the necessary dependencies:
+    ```
+    source venv/bin/activate
+    pip install -r requirements.txt 
+    ```
+5. To run the Streamlit app locally, simply execute:
+
+    ```streamlit run app.py```
+
+Once the app is running, enter http://localhost:8501 in your browser to load the streamlit app.
+
+
+**Enter a query such as:**
+
+-   "What is the stock price of AAPL?"
+-   "Show me the financials of Microsoft."
+-   "Give me the income statement for Amazon."
+
+The assistant will process the input and provide the appropriate response, fetching data from the APIs as needed.
+
+
+
+
